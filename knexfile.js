bc8e836184bc6257e76f954eeeacbd4274d4740b
kingtamaaj <kingtamaaj@gmail.com>
2019-06-26 18:46:59 -0400

    Setup and cohorts database

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullasDefault: true,
+
+    connection: {
+      filename: './data/lambda.db3',
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+};
